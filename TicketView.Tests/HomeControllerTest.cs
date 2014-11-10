1ededf0db8fa6a75dbbd218492a754b042f6b70a
nrogers <nrogers@getnetdriven.com>
2014-11-10 13:09:39 -0500

    Assembla controller not needed

--- TicketView.Tests/HomeControllerTest.cs
+++ TicketView.Tests/HomeControllerTest.cs
@@ -1,12 +1,15 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Web;
 using System.Web.Routing;
+using Newtonsoft.Json.Linq;
 using TicketView.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
 using System.Web.Mvc;
+using TicketView.Models;
 
 namespace TicketView.Tests
 {
@@ -79,5 +82,46 @@ namespace TicketView.Tests
 
             Assert.IsInstanceOfType(result, typeof(RedirectResult));
         }
+
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [UrlToTest("http://tickets.nick/")]
+        [DeploymentItem("TicketView.dll")]
+        public void ReauthorizeTokenTestWithAuthToken()
+        {
+            HomeController_Accessor target = new HomeController_Accessor();
+            HttpCookie cookie = new HttpCookie("AuthToken", "123");
+            string expected = "123";
+            string actual;
+
+            actual = target.ReauthorizeToken(cookie);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [UrlToTest("http://tickets.nick/")]
+        [DeploymentItem("TicketView.dll")]
+        public void ParseMilestonesTest()
+        {
+            string sampleResponse = System.IO.File.ReadAllText(@"..\..\SampleResponses\GetMilestones.txt");
+            JArray milestonesResponse = JArray.Parse(sampleResponse);
+            List<Milestone> actual = HomeController_Accessor.ParseMilestones(milestonesResponse);
+            Assert.IsTrue(actual.Count > 0);
+        }
+
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [UrlToTest("http://tickets.nick/")]
+        [DeploymentItem("TicketView.dll")]
+        public void ParseTicketsTest()
+        {
+            string sampleResponse = System.IO.File.ReadAllText(@"..\..\SampleResponses\GetTickets.txt");
+            JArray ticketsResponse = JArray.Parse(sampleResponse);
+            List<Ticket> actual = HomeController_Accessor.ParseTickets(ticketsResponse);
+            Assert.IsTrue(actual.Count > 0);
+        }
+
     }
 }
--- TicketView/Controllers/HomeController.cs
+++ TicketView/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json.Linq;
+using TicketView.Models;
 
 namespace TicketView.Controllers
 {
@@ -17,7 +18,10 @@ namespace TicketView.Controllers
             {
                 return RedirectToAction("Login");
             }
-            return View();
+
+            ViewModel vm = new ViewModel(ParseMilestones(GetMilestones()), "7260603", ParseTickets(GetTickets("7260603")));
+
+            return View(vm);
         }
 
         [HttpGet]
@@ -64,6 +68,113 @@ namespace TicketView.Controllers
             Response.Cookies.Add(new HttpCookie("RefreshToken", token["refresh_token"].Value<string>()) { Expires = DateTime.Now.AddDays(30) } );
 
             return RedirectToAction("Index");
+
+        }
+
+        private string ReauthorizeToken(HttpCookie cookie)
+        {
+            if (cookie != null)
+                return cookie.Value;
+
+            if (Request.Cookies["RefreshToken"] != null)
+            {
+                string tokenUrl = String.Format(Secrets.RefreshTokenUrl, Request.Cookies["RefreshToken"].Value);
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(tokenUrl);
+                request.Headers.Add(SharedMethods.GetBasicAuthorizationHeader());
+                request.UserAgent = "Nick's Ticket View/1.0";
+                request.ContentLength = 0;
+                request.Method = "POST";
+
+                string result;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = sr.ReadToEnd();
+                }
+
+                JObject token = JObject.Parse(result);
+                string newToken = token["access_token"].Value<string>();
+                Response.Cookies.Add(new HttpCookie("AuthToken", newToken) { Expires = DateTime.Now.AddSeconds(590) });
+
+                return newToken;
+            }
+
+            throw new UnauthorizedAccessException();
+        }
+
+        private JArray GetMilestones()
+        {
+            string url = String.Format("https://api.assembla.com/v1/spaces/{0}/milestones.json", Secrets.SpaceId);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add(SharedMethods.GetBearerAuthorizationHeader(ReauthorizeToken(Request.Cookies["AuthToken"])));
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string result;
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            return JArray.Parse(result);
+        }
+
+        private static List<Milestone> ParseMilestones(JArray milestonesResponse)
+        {
+            return (from JObject jo in milestonesResponse select new Milestone(jo)).ToList();
+        }
+
+        private JArray GetTickets(string milestoneId)
+        {
+            int page = 1;
+            JArray tickets = new JArray();
+            while (page <= 6)
+            {
+                string url = String.Format("https://api.assembla.com/v1/spaces/{0}/tickets/milestone/{1}.json?per_page=25&page={2}&ticket_status=all", Secrets.SpaceId, milestoneId, page++);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Headers.Add(SharedMethods.GetBearerAuthorizationHeader(ReauthorizeToken(Request.Cookies["AuthToken"])));
+                request.Method = "GET";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                string result;
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = sr.ReadToEnd();
+                }
+
+                if (String.IsNullOrWhiteSpace(result))
+                    break;
+
+                JArray t = JArray.Parse(result);
+                foreach (JObject jo in t.Where(a => a["assigned_to_id"].Value<string>() == Secrets.UserId))
+                {
+                    tickets.Add(jo);
+                }
+            }
+            return tickets;
+        }
+
+        private static List<Ticket> ParseTickets(JArray ticketsResponse)
+        {
+            return (from JObject jo in ticketsResponse select new Ticket(jo)).ToList();
+        }
+
+        public class ViewModel
+        {
+            public List<Milestone> Milestones { get; private set; }
+            public List<Ticket> Tickets { get; private set; }
+            public string SelectedMilestone { get; private set; }
+
+            public ViewModel(List<Milestone> milestones, string selectedMilestone = null, List<Ticket> tickets = null)
+            {
+                Milestones = milestones;
+                if (selectedMilestone != null)
+                {
+                    SelectedMilestone = selectedMilestone;
+                    Tickets = tickets;
+                }
+            }
 
         }
 
